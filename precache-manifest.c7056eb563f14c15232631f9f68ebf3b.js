819244e5e5c83c91d441fd09c6a1563ae10ef7db
Shubalub Kingz <atstay@gmail.com>
2018-12-20 21:38:42 -0700

    Updates

---
+++ precache-manifest.c7056eb563f14c15232631f9f68ebf3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a28e1b1b84e0357464e",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "6a28e1b1b84e0357464e",
+    "url": "/contactmanager/static/js/main.6a28e1b1.chunk.js"
+  },
+  {
+    "revision": "43af630d959377dfb957",
+    "url": "/contactmanager/static/css/1.f8d28b38.chunk.css"
+  },
+  {
+    "revision": "43af630d959377dfb957",
+    "url": "/contactmanager/static/js/1.43af630d.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "7baec0ea6afd27af12e9aad82eb8cb81",
+    "url": "/contactmanager/index.html"
+  }
+];
